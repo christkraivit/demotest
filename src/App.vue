b2cb6e6ee3d5c6d508f0ec89d78847ee338dc7a3
SonoSolitario <vittaya_vity@hotmail.com>
2019-06-28 16:13:00 +0700

    update navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <app-banner></app-banner>
+
+    <router-view/>
     <vue-progress-bar></vue-progress-bar>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    "app-banner": () => import("./components/Banner")
+  },
   mounted() {
     //  [App.vue specific] When App.vue is finish loading finish the progress bar
     this.$Progress.finish();
@@ -30,26 +31,119 @@ export default {
       this.$Progress.finish();
     });
   }
-}
+};
 </script>
 
 
 <style lang="scss">
+body {
+  background-color: #000000;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Kanit", sans-serif;
+  font-weight: 200;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #fff;
+
+  // background-image: url("./assets/wallpaper.jpg");
+  // background-size: inherit;
+  // background-attachment: fixed;
+  // background-position: center center;
+
+  .text-title {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    text-align: center;
+    background: -webkit-linear-gradient(#f9f6ef, #ebd070);
+    border-style: solid;
+    border-width: 1px;
+    background-clip: border-box;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: #ffb10017;
+    border-image: linear-gradient(
+        to left,
+        rgba(0, 0, 0, 0) 0,
+        #fcca6a 50%,
+        rgba(0, 0, 0, 0) 100%
+      )
+      100% 0 100% 0/0px 0 2px 0 stretch;
+    border-image-source: linear-gradient(
+      to left,
+      rgba(0, 0, 0, 0) 0px,
+      rgb(252, 202, 106) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    border-image-slice: 100% 0;
+    border-image-width: 0px 0 2px;
+    border-image-outset: initial;
+    border-image-repeat: stretch;
+    color: #ebd070 !important;
+  }
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+.btn-line {
+  background-color: #00b529 !important;
+  color: #fff !important;
+  &:hover {
+    background-color: #00a526 !important;
+    color: #fff !important;
+  }
+}
+
+.bg-theme {
+  background: linear-gradient(to bottom right, #db0000 10%, #000000 100%);
+}
+.separator {
+  height: 2px;
+  background: linear-gradient(
+    to right,
+    #bf953f,
+    #fcf6ba,
+    #b38728,
+    #fbf5b7,
+    #aa771c
+  );
+}
+a {
+  color: #fcf6ba;
+}
+
+a:hover {
+  text-decoration: none;
+  color: #b38728;
+}
+
+.text-gold {
+  color: #eec850;
+}
+
+/* entry-header */
+.entry-header {
+  border-bottom: 3px solid #b00020;
+  margin-bottom: 15px;
+  > .title {
+    background-color: #b00020;
+    margin: 0;
+    color: #fff;
+    display: inline-block;
+    padding: 5px 15px 5px 5px;
+  }
+  > .link {
+    float: right;
+    padding-top: 10px;
+    color: #b00020;
+  }
+}
+
+.entry-header2 {
+  margin-bottom: 15px;
+  > .title {
+    background-color: #b00020;
+    margin: 0;
+    color: #fff;
+    display: inline-block;
+    padding: 5px 15px 5px 5px;
   }
 }
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="banner py-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="../assets/logo.png" style="max-height:64px" alt class="img-fluid ">
+        </div>
+        <div class="col-md-6 text-right">
+          <button @click="linkLine()" class="btn btn-register mr-2">สมัครสมาชิก</button>
+          <button @click="login()" class="btn btn-login ml-2">เข้าสู่ระบบ</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@mixin btn-hover {
+  &:hover {
+    color: #fff;
+    box-shadow: 0 0 6px 0 rgb(236, 0, 0);
+    background: rgba(0, 0, 0, 0.1);
+    transition: all 0.2s cubic-bezier(0, 0, 0.1, 1.97);
+    &:before {
+      animation: shine 0.5s 0s linear;
+    }
+  }
+}
+
+.banner {
+    .col-md-6{
+        @media (max-width: 768px){
+            text-align: center !important;
+        }
+    }
+  .btn-register {
+    padding-left: 24px;
+    padding-right: 24px; 
+    border: 1px solid rgba(225, 0, 0, 0.94);
+    background: linear-gradient(to bottom, #f30505 0%, #8a0101 100%);
+    color: #ffffff;
+    border-radius: 7px; 
+    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.44);
+    @include btn-hover;
+  }
+
+  .btn-login {
+    padding-left: 40px;
+    padding-right: 40px; 
+    border-radius: 7px; 
+    background: linear-gradient(to bottom, #707070 0%, #111111 100%);
+    color: #fff;
+    border: 1px solid #b0b0b0;
+    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.14);
+    @include btn-hover;
+  }
+}
+</style>
